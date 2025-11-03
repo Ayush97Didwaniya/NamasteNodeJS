86348b616cade971084cefd9a8197f0a1b6c28ef
AyushDidwaniya <didwaniya.ayush@gmail.com>
2025-11-03 23:07:29 +0530

    Exploring get post delete

--- Project/DevTinderBackend/src/app.js
+++ Project/DevTinderBackend/src/app.js
@@ -2,10 +2,31 @@ const express = require("express");
 
 const app = express();
 
-app.use((req, res) => {
-	res.send("Hello from the server new");
+app.get("/hello", (req, res) => {
+	res.send("Hello get api called");
 });
 
+app.get("/user", (req, res) => {
+	res.send({ name: "ayush" });
+});
+
+app.post("/user", (req, res) => {
+	res.send("Data Successfully saved to the database");
+});
+
+app.delete("/hello", (req, res) => {
+	res.send("Hello delete api called");
+});
+
+// this will handle all the api calls to /hello
+app.use("/hello", (req, res) => {
+	res.send("Hello api called");
+});
+
+// this will handle only get api calls to /hello
+app.use("test", (req, res) => {
+	res.send("Hello from the server new");
+});
 // app.use("/test", (req, res) => {
 // 	res.send("test");
 // });
