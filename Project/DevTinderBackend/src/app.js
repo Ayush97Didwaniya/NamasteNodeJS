0b682955b2c0a1591527e364527e3d507c25a62d
AyushDidwaniya <didwaniya.ayush@gmail.com>
2025-11-12 22:29:23 +0530

    user API's added

--- Project/DevTinderBackend/src/app.js
+++ Project/DevTinderBackend/src/app.js
@@ -29,6 +29,68 @@ app.post("/signup", async (req, res) => {
 	}
 });
 
+// Get user by email
+app.get("/user", async (req, res) => {
+	const userEmail = req.body.emailId;
+
+	try {
+		const users = await User.findOne({ emailId: userEmail });
+		if (users.length === 1) {
+			res.status(404).send("User not found");
+		} else {
+			res.send(users);
+		}
+	} catch (err) {
+		res.status(400).send("Something went wrong");
+	}
+});
+// Feed API - get /feed - get all the users from the database
+app.get("/feed", async (req, res) => {
+	try {
+		const users = await User.find({});
+		if (users.length === 1) {
+			res.status(404).send("User not found");
+		} else {
+			res.send(users);
+		}
+	} catch (err) {
+		res.status(400).send("Something went wrong");
+	}
+});
+
+// delet a user from the database
+app.delete("/user", async (req, res) => {
+	const userId = req.body.userId;
+	try {
+		console.log("userId", userId);
+		// const user = await User.findByIdandDelete({ _id: userId });
+		const user = await User.findByIdAndDelete(userId);
+		res.send("User deleted successfully");
+	} catch (err) {
+		res.status(400).send("Something went wrong");
+	}
+});
+
+//  Update data of user
+app.patch("/user", async (req, res) => {
+	const userId = req.body.userId;
+	const data = req.body;
+	try {
+		// await User.findByIdAndUpdate({ _id: userId }, data, {
+		// 	returnDocument: "before",
+		// });
+		// await User.findByIdAndUpdate({ _id: userId }, data, {
+		// 	returnDocument: "before",
+		// });
+		// by default it return before the update value;
+		const user = await User.findByIdAndUpdate({ _id: userId }, data);
+		console.log(user);
+		res.send("User Updated successfully");
+	} catch (err) {
+		res.status(400).send("Something went wrong");
+	}
+});
+
 connectDB()
 	.then(() => {
 		console.log("Database connection establised...");
