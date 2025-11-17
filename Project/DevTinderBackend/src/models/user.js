11f03aa586743130472c03baaaff8fdce67ecbe9
AyushDidwaniya <didwaniya.ayush@gmail.com>
2025-11-18 00:12:47 +0530

    Exploring validator library

--- Project/DevTinderBackend/src/models/user.js
+++ Project/DevTinderBackend/src/models/user.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const validator = require("validator");
 
 const userSchema = mongoose.Schema(
 	{
@@ -15,10 +16,20 @@ const userSchema = mongoose.Schema(
 			required: true,
 			unique: true,
 			trim: true,
+			validate(value) {
+				if (!validator.isEmail(value)) {
+					throw new Error("Invalid Email address: " + value);
+				}
+			},
 		},
 		password: {
 			type: String,
 			required: true,
+			validate(value) {
+				if (!validator.isStrongPassword(value)) {
+					throw new Error("Enter a strong password: " + value);
+				}
+			},
 		},
 		age: {
 			type: Number,
@@ -34,6 +45,11 @@ const userSchema = mongoose.Schema(
 		photoUrl: {
 			type: String,
 			default: "https://tamilnaducouncil.ac.in/dummy-avatar/",
+			validate(value) {
+				if (!validator.isURL(value)) {
+					throw new Error("Invalid Photo Url" + value);
+				}
+			},
 		},
 		about: {
 			type: String,
